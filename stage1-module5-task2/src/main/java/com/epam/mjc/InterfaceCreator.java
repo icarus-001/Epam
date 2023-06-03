753420ce3c11718ef3fb6558fe180d94adfecf88
Andre Khachatryan <43244738+icarus-001@users.noreply.github.com>
2023-06-03 18:51:07 +0400

    Added from local

---
+++ stage1-module5-task2/src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -0,0 +1,17 @@
+package com.epam.mjc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InterfaceCreator {
+
+    public Operation<Integer> divideBy(Integer divider) {
+        return x -> {
+            List<Integer> list = new ArrayList<>();
+            for (Integer n : x) {
+                list.add(n/divider);
+            }
+            return list;
+        };
+    }
+}
