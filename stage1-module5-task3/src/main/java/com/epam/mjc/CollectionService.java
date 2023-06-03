fcac5f30c4e0b02d53a941c5765af6ff3794bc6a
Andre Khachatryan <43244738+icarus-001@users.noreply.github.com>
2023-06-03 18:51:33 +0400

    Added from local

---
+++ stage1-module5-task3/src/main/java/com/epam/mjc/CollectionService.java
@@ -0,0 +1,34 @@
+package com.epam.mjc;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class CollectionService {
+
+    public List<Integer> filterEvenNumbers(List<Integer> list) {
+        return list.stream()
+                .filter(num -> num % 2 == 0)
+                .collect(Collectors.toList());
+    }
+
+
+    public List<String> toUpperCaseCollection(List<String> list) {
+        return list.stream().map(String::toUpperCase).collect(Collectors.toList());
+    }
+
+    public Optional<Integer> findMax(List<Integer> list) {
+        return list.stream().max(Comparator.naturalOrder());
+    }
+
+    public Optional<Integer> findMin(List<List<Integer>> list) {
+        return list.stream()
+                .flatMap(List::stream)
+                .min(Integer::compareTo);
+    }
+
+    public Integer sum(List<Integer> list) {
+        return list.stream().reduce(0, Integer::sum);
+    }
+}
